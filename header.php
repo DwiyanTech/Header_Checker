450676e6386821f5a8429285aab412db232872bb
DwiyanTech <47693495+DwiyanTech@users.noreply.github.com>
2019-03-26 10:51:42 -0500

    Update V.1 To V.2

--- header.php
+++ header.php
@@ -1,41 +1,108 @@
 <?php
-if(!empty($argv[1])){
+echo "####################################\n";
+echo "#     Lunatic Header Checker V.2   #\n";
+echo "#          LunaticTech.XyZ         #\n";
+echo "#     ICWR-TECH ||  Ahmad Dwiyan   #\n";
+echo "####################################\n";
+echo "\nRun 'help' For Command list \n";  
+while(true){
+echo "Command => ";
+$cmd = trim(fgets(STDIN));
 
-echo"###################################\n";
-echo "#          Header Checker       #\n";
-echo "#         By Lunatic-Tech       #\n";
-echo "# icwr-tech.id | lunatictech.xyz #\n";
-echo "##################################\n";
+if($cmd == "help"){
 
-
-   $url = $argv[1];
-   echo "Checkking Header...\n";
-$cek = curl_init($url);
-curl_setopt($cek,CURLOPT_HEADER,true);
-curl_setopt($cek,CURLOPT_CONNECTTIMEOUT,10);
-curl_setopt($cek,CURLOPT_NOBODY,true);
-curl_setopt($cek,CURLOPT_RETURNTRANSFER,true);
-$header = curl_exec($cek);
-
-if(!$header){
-echo"Check Your Connection OR URL FIRST !!!!\n";
+echo "\n|+List Command+|\nresponse = Checking Header Response Without Code And Verbose False \ncode = Only Check Header Response Code\nheader = Checking Header Response And SCRIPT \nInfo =  All Information Of Headers Request(Verbose ON) \nlunatic = Information Tools ! \nexit = Exit Tools\n\n";
 
 
+
+} 
+if($cmd == "response"){
+echo "URL => ";
+$url = trim(fgets(STDIN));
+
+	$ch = curl_init();
+	curl_setopt($ch,CURLOPT_URL,$url);
+	curl_setopt($ch,CURLOPT_HEADER,true);
+	curl_setopt($ch,CURLOPT_VERBOSE,false);
+	curl_setopt($ch,CURLOPT_NOBODY,true);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	$cek = curl_exec($ch);
+	curl_close($ch);
+
+if($cek) {
+echo $cek;
 } else {
-    echo "Connected !!!\n";
-echo $header;
-echo"\n=========== End Of Header ===========";
+echo "Check Your URL And Connection First !!";
+}
 }
 
-} else {
-    echo"###################################\n";
-    echo "#          Header Checker       #\n";
-    echo "#         By Lunatic-Tech       #\n";
-    echo "# icwr-tech.id | lunatictech.xyz #\n";
-    echo "##################################\n";
-    
-    echo "php header.php <URL>";
+if($cmd == "code"){
+
+echo "Url => ";
+$url = trim(fgets(STDIN));
+
+	$ch = curl_init($url);
+	curl_setopt($ch,CURLOPT_HEADER,true);
+	curl_setopt($ch,CURLOPT_NOBODY,true);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	$code = curl_exec($ch);
+	$kode = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+	curl_close($ch);
+
+echo "\nYour URL : $url \nResponse Code => $kode\n\n";
+
+
 }
+if($cmd == "headscript"){
+
+echo "URL => ";
+$url = trim(fgets(STDIN));
+
+	$ch = curl_init($url);
+	curl_setopt($ch,CURLOPT_VERBOSE,true);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	$code = curl_exec($ch);
+	
+	curl_close($ch);
+	
+	echo $code."\n\n";
+	
+}
+if($cmd == "info"){
+
+echo "URL => ";	
+$url = trim(fgets(STDIN));
+	
+	$ch = curl_init($url);
+	curl_setopt($ch,CURLOPT_HEADER,true);
+	curl_setopt($ch,CURLOPT_NOBODY,true);
+	curl_setopt($ch,CURLOPT_VERBOSE,true);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	$code = curl_exec($ch);
+	
+	curl_close($ch);
+	
+	echo $code."\n\n";
+	
+	
+}
+if($cmd == "lunatic"){
+	
+echo "| LUNATIC HEADER CHECKER V.2  By Lunatictech.XyZ  |\n";
+echo "| Greetz Thanks To Afrizal F A ICWR-TECH.ID       |\n"; 
+echo "| Check Our Website Or Github To Check For Update |\n";	
+echo "|        https://github.com/DwiyanTech            |\n"; 
+
+	
+}
+if($cmd == "exit"){
+	
+	break;
+	
+}
+
+}
+
 
 
 ?>
